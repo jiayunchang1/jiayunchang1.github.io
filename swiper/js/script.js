72360bc4cbd7288b43c12e181f52a567a4d2c619
jiayunchang1 <486626667@qq.com>
2017-12-05 10:11:59 +0800

    新增swiper

---
+++ swiper/js/script.js
@@ -0,0 +1,14 @@
+/**
+ * Created by lenovo on 17/12/5 005.
+ */
+var swiper = new Swiper('.swiper-container', {
+    direction: 'vertical',
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+
+    },
+});
